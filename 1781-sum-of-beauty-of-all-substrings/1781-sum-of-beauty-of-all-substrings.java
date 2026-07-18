3af206c699e36944c233e0d5ec4143bc99e19bed
Sudip Sau <sudipsau9818@gmail.com>
2026-07-18 17:46:08 +0530

    Time: 59 ms (91.93%), Space: 44.2 MB (59.47%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -6,19 +6,17 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
 
+            int[] freq = new int[26];
+
             for (int j = i; j < n; j++) {
 
-                int[] freq = new int[26];
-
-                // Count frequencies for substring s[i...j]
-                for (int k = i; k <= j; k++) {
-                    freq[s.charAt(k) - 'a']++;
-                }
+                freq[s.charAt(j) - 'a']++;
 
                 int maxFreq = 0;
                 int minFreq = Integer.MAX_VALUE;
 
                 for (int k = 0; k < 26; k++) {
+
                     if (freq[k] > 0) {
                         maxFreq = Math.max(maxFreq, freq[k]);
                         minFreq = Math.min(minFreq, freq[k]);
