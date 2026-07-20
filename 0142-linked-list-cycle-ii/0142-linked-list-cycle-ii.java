4db40cfc843f784a16e9a06bb2f4b98aa8a037d7
Sudip Sau <sudipsau9818@gmail.com>
2026-07-20 21:53:19 +0530

    Time: 6 ms (10.51%), Space: 46.8 MB (13.57%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        HashSet<ListNode> set = new HashSet<>();
+
+        while (head != null) {
+
+            if (set.contains(head))
+                return head;
+
+            set.add(head);
+
+            head = head.next;
+        }
+
+        return null;
+    }
+}
