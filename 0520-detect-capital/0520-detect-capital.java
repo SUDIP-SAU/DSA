ae3553cd87d367d52e15a2f09831b59b5d98e6f1
Sudip Sau <sudipsau9818@gmail.com>
2026-08-22 22:15:17 +0530

    Time: 1 ms (89.44%), Space: 42.9 MB (89.73%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        
+        int count = 0;
+
+        for(int i = 0; i < word.length(); i++) {
+
+            if(Character.isUpperCase(word.charAt(i)))
+                count++;
+            
+
+        }
+
+        if(count == 1 && Character.isUpperCase(word.charAt(0))){
+            return true;
+        }
+
+        else if(count == 0){
+
+            return true;
+        } 
+
+        else if(count == word.length()){
+
+            return true;
+        }  
+
+        return false; 
+    }
+
+}
