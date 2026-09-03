e6ec4de7ba0681e0bd990ddd001df421bfbd34c7
Sudip Sau <sudipsau9818@gmail.com>
2026-09-03 22:59:08 +0530

    Time: 1 ms (99.77%), Space: 42.8 MB (92.95%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,23 +1,16 @@
 class Solution {
     public char findTheDifference(String s, String t) {
 
-        int[] freq = new int[26];
+        char ans = 0;
 
         for (char ch : s.toCharArray()) {
-            freq[ch - 'a']++;
+            ans ^= ch;
         }
 
         for (char ch : t.toCharArray()) {
-            freq[ch - 'a']--;
+            ans ^= ch;
         }
 
-        for (int i = 0; i < 26; i++) {
-
-            if (freq[i] != 0) {
-                return (char) ('a' + i);
-            }
-        }
-
-        return '\0';
+        return ans;
     }
 }
